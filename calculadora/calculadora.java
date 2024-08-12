afb7cbc56f4b3eba8beb4ce15d58cf1d018d877b
Pedro Alegria Fiamoncini <93059776+Fiamoncini@users.noreply.github.com>
2024-08-12 10:26:00 -0300

    Update calculadora.java

--- calculadora/calculadora.java
+++ calculadora/calculadora.java
@@ -1,19 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package calculadora;
-
-/**
- *
- * @author pedro
- */
 public class calculadora {
     int num1;
     int num2;
     int resultado;
-    
-    
+        
     public int somar(){
         return num1 + num2;
     }
